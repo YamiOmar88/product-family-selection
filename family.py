56e6af32e18a2055fda6394387e38e84e2f3d645
Yamila Omar <yamila.omar@uni.lu>
2019-04-24 09:19:58 +0200

    docstrings added to family.py

--- family.py
+++ family.py
@@ -4,19 +4,35 @@
 
 class Family:
     def __init__(self):
-        ''''''
+        '''Initializes Family object. All Family objects are initialized with three empty
+        attributes: 
+        - base: is the set of nodes (workstations) for this family. Default: empty set.
+        - family_members: is a list of paths that belong to this family. Default: empty list.
+        - family_count: is an integer accounting for the number of processed items that 
+        belong to this family. Default: 0.
+        Users are expected to call on the find_family method to populate these attributes.'''
         self.base = set([])
         self.family_members = []
         self.family_count = 0
     
     def _log_process(self, text):
-        ''' '''
+        '''This internal function allows to create a text file to log data from the 
+        find_family method. It was used during module development to understand how the 
+        method functioned.'''
         filename = "family_search_log.txt"
         with open(filename, 'a') as f:
             f.write(text)
     
     def find_family(self, sets_list, threshold=0.7, log=False):
-        ''''''
+        '''This method finds the base, family_members and family_count of the Family object. 
+        It requires the following input variables: 
+        - sets_list: a list of path sets in which each element is a tuple (c, p) where c is 
+        the path count and p is the path set.
+        - threshold: float between 0 and 1. It is needed to calculate if a path set Jaccard 
+        similarity is above or below threshold. Default: 0.7.
+        - log: boolean indicating if a log text file should be written. Default: False.
+        This function returns a list of unused sets from the input sets_list.'''
+        sets_list.sort(reverse = True)
         for item in sets_list:
             text = "\nProcessing item: {}\n".format(item)
             path, count = item[1], item[0]
@@ -46,11 +62,12 @@ class Family:
         
 class Similarity:
     def __init__(self, S, T):
-        ''' '''
+        '''Initializes Similarity object. Input variables: S and T. They are iterables that can 
+        be converted to sets.'''
         self.S = set(S)
         self.T = set(T) 
         
     @property
     def jaccard(self):
-        ''' '''
+        '''Property that calculates the Jaccard Similarity of sets S and T.'''
         return len(self.S.intersection(self.T)) / len(self.S.union(self.T))
